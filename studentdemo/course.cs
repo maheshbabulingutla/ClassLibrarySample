5611470eb7aa3ff7817c1688a77b1fae2d03ccd3
MaheshBabu lingutla <lmaheshchowdary@gmail.com>
2016-09-26 02:11:24 -0500

    Class Libary Sample file updated

---
+++ studentdemo/course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentdemo
+{
+    class course
+    {
+        public int courseid { get; set; }
+        public string coursename { get; set; }
+    }
+}
---
+++ studentdemo/faculty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentdemo
+{
+    class faculty:person
+    {
+        public int facultyid { get; set; }
+    }
+}
